1bb1fac31848c5ffe13d53b556ab1230dad9e2f8
Filip Skřiváček <filskri019@pslib.cz>
2022-06-16 23:55:16 +0200

    Add project files.

---
+++ RockPaperScissors_Skrivacek/Convertor/Convertor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace RockPaperScissors_Skrivacek.Convertor
+{
+    internal class Convertor : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int number = (int)value;
+            switch (number)
+            {
+                case 3:
+                    return Application.Current.MainWindow.FindResource("scissors");
+                case 2:
+                    return Application.Current.MainWindow.FindResource("paper");
+                case 1:
+                    return Application.Current.MainWindow.FindResource("rock");
+                default:
+                    return new BitmapImage();
+            }
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ RockPaperScissors_Skrivacek/ViewModels/MainViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfRPS.ViewModels;
+
+namespace RockPaperScissors_Skrivacek.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+       
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool _running;
+        private int _userscore;
+        private int _pcscore;
+        private Random _rnd = new Random();
+        private Item _user;
+        private Item _pc;
+
+        public MainViewModel()
+        {
+            PcScore = 0;
+            UserScore = 0;
+            User = Item.Nothing;
+            Pc = Item.Nothing;        
+            Running = false;
+
+            StartCommand = new RelayCommand(
+                () =>
+                {
+                    PcScore = 0;
+                    UserScore = 0;
+                    User = Item.Nothing;
+                    Pc = Item.Nothing;
+                    Running = true;
+                    StartCommand.RaiseCanExecuteChanged();
+                    GameCommand.RaiseCanExecuteChanged();
+
+                },
+                () =>
+                {
+                    return !Running;
+                }
+              );
+
+            GameCommand = new ParametrizedRelayCommand<string>(
+                (parameter) =>
+                {
+                    User = (Item)Convert.ToInt32(parameter);
+                    Pc = (Item)_rnd.Next(1, Enum.GetNames(typeof(Item)).Length);
+
+                    if (User == Item.Rock && Pc == Item.Scissors ||
+                        User == Item.Paper && Pc == Item.Rock ||
+                        User == Item.Scissors && Pc == Item.Paper)
+                    {
+                        UserScore = +1; /*!!!!!!!!!!!!!!!!*/
+                    }
+                    else if (Pc == Item.Rock && User == Item.Scissors ||
+                        Pc == Item.Paper && User == Item.Rock ||
+                       Pc == Item.Scissors && User == Item.Paper)
+                    {
+                        PcScore = +1;
+                    }
+
+                    if (UserScore == 5 || PcScore == 5)
+                    {
+                        Running = false;
+                    }
+
+                    StartCommand.RaiseCanExecuteChanged();
+                    GameCommand.RaiseCanExecuteChanged();
+                },
+                (parameter) =>
+                {
+                    return Running;
+                }
+              );
+                 
+
+        }
+
+        public Item User { get { return _user; } set { _user = value; NotifyPropertyChanged(); } }
+        public int UserScore { get { return _userscore; } set { _userscore = value; NotifyPropertyChanged(); } }
+        public Item Pc { get { return _pc; } set { _pc = value; NotifyPropertyChanged(); } }
+        public int PcScore { get { return _pcscore; } set { _pcscore = value; NotifyPropertyChanged(); } }
+        public bool Running { get { return _running; } set { _running = value; NotifyPropertyChanged(); } }
+        public RelayCommand StartCommand { get; set; }
+        public ParametrizedRelayCommand<string> GameCommand { get; set; }
+
+        internal enum Item
+        {
+            Nothing = 0,
+            Rock = 1,
+            Paper = 2,
+            Scissors = 3
+        }
+
+    }
+}
